4a9a508d83982d9be123431a95003f3f5a670bd0
egr <egr@zengularity.com>
2015-11-11 22:37:36 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject inccup "0.0.1"
+  :description "Hiccup enhanced by an incremental DOM implementation"
+  :url "https://github.com/EwenG/viccup"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]])
