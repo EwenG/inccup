02703d12c34c8c2e2eddc325ec1817caab78afe5
ewen <ewen.grosjean@free.fr>
2016-04-28 22:26:24 +0200

    update tests

--- test/ewen/inccup/incremental/core_test.cljs
+++ test/ewen/inccup/incremental/core_test.cljs
@@ -83,23 +83,75 @@
                "prev-result" ["p" {} 3 ["p" {} 3] ["p" {} 4]],
                "params" [3 4]}])))))
 
-  #_(testing "dynamic-cache"
-        (binding [*cache* (init-cache)]
-          (reset! cache-seq [])
-          (let [res1 (template3 [1 2])
-                _ (prn res1)
-                _ (swap! cache-seq conj (js->clj *cache*))
-                res2 (template3 [1 2])
-                _ (prn res2)
-                res3 (template3 [1 2 3])
-                _ (prn res3)
-                _ (swap! cache-seq conj (js->clj *cache*))
-                res4 (template3 [6])
-                _ (prn res4)
-                _ (swap! cache-seq conj (js->clj *cache*))]
-            (identical? res1 res2)
-            (is (not= res1 res3 res4))
-            (prn @cache-seq)))))
+  (testing "dynamic-cache"
+      (binding [*cache* (init-cache)]
+        (reset! cache-seq [])
+        (let [res1 (template3 [1 2])
+              _ (prn res1)
+              _ (clean-cache *cache*)
+              _ (swap! cache-seq conj (js->clj *cache*))
+              res2 (template3 [1 2])
+              _ (prn res2)
+              _ (clean-cache *cache*)
+              res3 (template3 [1 2 3])
+              _ (prn res3)
+              _ (clean-cache *cache*)
+              _ (swap! cache-seq conj (js->clj *cache*))
+              res4 (template3 [6])
+              _ (prn res4)
+              _ (clean-cache *cache*)
+              _ (swap! cache-seq conj (js->clj *cache*))]
+          (identical? res1 res2)
+          (is (not= res1 res3 res4))
+          (is
+           (= @cache-seq
+              '[{"sub-cache"
+                 [{"dynamic-counter" 0, "dynamic-array" []}
+                  {"dynamic-counter" 2,
+                   "dynamic-array"
+                   [{"sub-cache" [],
+                     "version" 1,
+                     "params" [1],
+                     "prev-result" ["p" {} 1]}
+                    {"sub-cache" [],
+                     "version" 1,
+                     "params" [2],
+                     "prev-result" ["p" {} 2]}]}],
+                 "version" 1,
+                 "params" [[1 2]],
+                 "prev-result" ["p" {} 2 (["p" {} 1] ["p" {} 2])]}
+                {"sub-cache"
+                 [{"dynamic-counter" 0, "dynamic-array" []}
+                  {"dynamic-counter" 3,
+                   "dynamic-array"
+                   [{"sub-cache" [],
+                     "version" 3,
+                     "params" [1],
+                     "prev-result" ["p" {} 1]}
+                    {"sub-cache" [],
+                     "version" 3,
+                     "params" [2],
+                     "prev-result" ["p" {} 2]}
+                    {"sub-cache" [],
+                     "version" 3,
+                     "params" [3],
+                     "prev-result" ["p" {} 3]}]}],
+                 "version" 3,
+                 "params" [[1 2 3]],
+                 "prev-result"
+                 ["p" {} 3 (["p" {} 1] ["p" {} 2] ["p" {} 3])]}
+                {"sub-cache"
+                 [{"dynamic-counter" 0, "dynamic-array" []}
+                  {"dynamic-counter" 1,
+                   "dynamic-array"
+                   [{"sub-cache" [],
+                     "version" 4,
+                     "params" [6],
+                     "prev-result" ["p" {} 6]}]}],
+                 "version" 4,
+                 "params" [[6]],
+                 "prev-result" ["p" {} 1 (["p" {} 6])]}]))))))
+
 
 
 
@@ -111,6 +163,21 @@
 
 (comment
 
+  (defhtml template1 [x] [:p {} x])
+  (defhtml template3 [x] [:p {} (count x) (for [y x] (template1 y))])
 
+  (binding [*cache* (init-cache)]
+
+    (let [res1 (template3 [1 2])
+          _ (prn res1)
+          res2 (template3 [1 2])
+          _ (prn res2)
+          res3 (template3 [1 2 3])
+          _ (prn res3)
+          res4 (template3 [6])
+          _ (prn res4)]
+      res4
+      (clean-cache *cache*)
+      (js->clj *cache*)))
 
   )
