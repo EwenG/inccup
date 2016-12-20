2308fa47f7a99ed2c80551df180cd8a811784323
ewen <ewen.grosjean@free.fr>
2016-12-20 22:58:21 +0100

    update clj/cljs dependencies, add a lein-cljs build profile

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src"]
   :test-paths ["test"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha4"]
-                 [org.clojure/clojurescript "1.9.36"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.293"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [backtick "0.3.3"]
                                   [org.clojure/data.xml "0.0.8"]]}}
@@ -19,4 +19,13 @@
              "ewen.inccup.tests-runner/run-gen-reconciliation-tests"
              :project/gen-tests-runs-nb]
             "compiler-tests"
-            ["run" "-m" "ewen.inccup.tests-runner/run-compiler-tests"]})
+            ["run" "-m" "ewen.inccup.tests-runner/run-compiler-tests"]}
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src-cljs"]
+              :compiler {
+                         :output-to "target/cljs/main-o.js"
+                         :optimizations :advanced
+                         :pseudo-names true
+                         :pretty-print true}}]})
